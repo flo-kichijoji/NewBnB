b3b82cafc281562c8eb1ea09217c3ad9cf39e180
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-13 16:54:22 +0100

    Added routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :flats do
+    resources :bookings, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
+  end
 end
