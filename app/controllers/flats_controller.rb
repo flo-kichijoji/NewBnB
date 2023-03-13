73a395957c874ec96950ff4f75c84a4bff85bd7c
FLO KICHIJOJI <flokichijoji@outlook.fr>
2023-03-13 17:37:38 +0100

    AddFlatController

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,21 @@
 class FlatsController < ApplicationController
+  # This action uses query string parameters because it gets run
+  # by an HTTP GET request, but this does not make any difference
+  # to how the parameters are accessed. The URL for
+  # this action would look like this to list activated
+  # flats: /flats?status=activated
+  def index
+    if params[:status] == "activated"
+      @flats = Flat.activated
+    else
+      @flats = Flat.inactivated
+    end
+  end
+
+  # This action uses POST parameters. They are most likely coming
+  # from an HTML form that the user has submitted. The URL for
+  # this RESTful request will be "/flats", and the data will be
+  # sent as part of the request body.
   def create
     @flat = Flat.new(flat_params)
     if @flat.save
@@ -6,5 +23,12 @@ class FlatsController < ApplicationController
     else
       render :new
     end
+  end
+
+  private
+
+  # Not sure of this part below <flo>
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price, :capacity, :availability)
   end
 end
