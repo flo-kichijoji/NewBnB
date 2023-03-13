75c79ebb51df51aa291309a4f0c26dbc718ca0e7
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-13 22:00:53 +0100

    Mispelling found in model corrected

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ Review.destroy_all
 User.destroy_all
 
 puts "Creating user..."
-user = User.create! ({email: "potato@gmail.com"})
+user = User.create! ({id: 1, email: "potato@gmail.com", password: "123456"})
 puts "Creating flat..."
-flat = Flat.create!({name: "Treehouse in the forest", address: "Chamigny, Île-de-France, France", description: "Nice treehouse", price: 120, capacity: 4, user_id: 1})
+flat = Flat.create!({name: "Treehouse in the forest", address: "Chamigny, Île-de-France, France", description: "Nice treehouse", price: 120, capacity: 4, user_id: 1, availability: true})
 
 puts "Finished!"
