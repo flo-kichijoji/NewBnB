3c9070a3b66067a57a992d80c76e27ba841771dc
FLO KICHIJOJI <flokichijoji@outlook.fr>
2023-03-13 17:13:46 +0100

    AddFlatController

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,10 @@
 class FlatsController < ApplicationController
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @flat, notice: 'Successfully created an flat.'
+    else
+      render :new
+    end
+  end
 end
