8729681115690d1e2aa87d2ad8ff268f00049bae
FLO KICHIJOJI <flokichijoji@outlook.fr>
2023-03-13 16:35:55 +0100

    EditControllers an d fix destroyProb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  validates :name, :last_name, :first_name, :email, :phone_number, presence: true
-
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
