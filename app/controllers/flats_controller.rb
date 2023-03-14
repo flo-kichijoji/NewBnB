a208e811247ba93c0676d1db4a8889c10286a19e
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-03-14 10:34:54 +0100

    edit flats commit

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,26 @@
 class FlatsController < ApplicationController
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    if Flat.update(flat_params)
+      redirect_to @flat
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price, :capacity, :availability)
+  end
 end
