c48395ec635a54c0c7a5bf12c2bae7b082f24818
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-15 14:00:24 +0100

    Added photos in Flats database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_103502) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_15_125446) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_103502) do
     t.bigint "user_id", null: false
     t.float "latitude"
     t.float "longitude"
+    t.text "photo"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
