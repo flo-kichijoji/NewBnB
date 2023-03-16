97c51883af3857399428d7f1adfddc8e7ead74df
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-03-16 17:11:16 +0100

    added new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,19 @@ puts "Creating flat..."
 flat = Flat.create!({ name: "Tent in the woods", address: "Perugia, Umbria, Italy", description: "Nice tent", price: 50, capacity: 2, user_id: 1, availability: true, photo: "https://images.unsplash.com/photo-1624923686627-514dd5e57bae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" })
 puts "Creating flat..."
 flat = Flat.create!({ name: "Underwater submarine", address: "Coron, MIMAROPA, Philippines", description: "Nice submarine", price: 370, capacity: 4, user_id: 2, availability: true, photo: "https://images.unsplash.com/photo-1632692608995-bee322df2a3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" })
-
+puts "Creating flat..."
+flat = Flat.create!({ name: "Modern Loft in Downtown", address: "New York, NY, USA", description: "Beautiful loft with a city view", price: 250, capacity: 2, user_id: 1, availability: true, photo: "https://images.unsplash.com/photo-1594872653896-21c0b452d605?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80" })
+puts "Creating flat..."
+flat = Flat.create!({ name: "Seaside Villa", address: "Cancun, Mexico", description: "Luxurious villa with ocean view", price: 500, capacity: 6, user_id: 1, availability: true, photo: "https://images.unsplash.com/photo-1631645033513-15144f920557?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80" })
+puts "Creating flat..."
+flat = Flat.create!({ name: "Mountain Cabin", address: "Banff, Alberta, Canada", description: "Cozy cabin in the Rocky Mountains", price: 180, capacity: 4, user_id: 1, availability: true, photo: "https://images.unsplash.com/photo-1482192505345-5655af888cc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1856&q=80" })
+puts "Creating flat..."
+flat = Flat.create!({ name: "Beachfront Apartment", address: "Miami, FL, USA", description: "Stunning apartment with ocean view", price: 300, capacity: 4, user_id: 1, availability: true, photo: "https://images.unsplash.com/photo-1561799461-445437ea576b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" })
+puts "Creating flat..."
+flat = Flat.create!({ name: "Charming Cottage", address: "Cotswolds, England", description: "Quaint cottage in the English countryside", price: 150, capacity: 2, user_id: 1, availability: true, photo: "https://images.unsplash.com/photo-1498176126716-a01bc267f662?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1924&q=80" })
+puts "Creating flat..."
+flat = Flat.create!({ name: "Rustic Farmhouse", address: "Tuscany, Italy", description: "Traditional farmhouse with a rustic feel", price: 200, capacity: 6, user_id: 1, availability: true, photo: "https://images.unsplash.com/photo-1543372165-23cb16764356?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" })
+puts "Creating a flat..."
+flat = Flat.create!({ name: "Luxury Penthouse", address: "Dubai, UAE", description: "Spacious penthouse with stunning views", price: 1000, capacity: 8, user_id: 1, availability: true, photo: "https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" })
 
 puts "Finished!"
